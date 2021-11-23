ce4cb5d30ca470cc64cac868f07ea01d8fedbd73
Corban <sonnemanc@gmail.com>
2021-11-23 09:43:32 -0700

    commit tree 8b9bc770c7b60a571ad0109cc04842fc98f86a40

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        erb :index
+    end
+
+    configure do
+        enable :sessions
+        set :session_secret, 'flatiron'
+    end
+
+    post '/checkout' do
+        session[:item] = params[:item]
+        @session = session
+        erb :checkout
+    end
 end
